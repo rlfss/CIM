d59527047aa75a283e0e021cd988ecca686cb0a6
med-sahara <55658971+med-sahara@users.noreply.github.com>
2020-01-05 15:51:33 +0200

    Add files via upload

--- hr_employee_info/models/hr_employee_info.py
+++ hr_employee_info/models/hr_employee_info.py
@@ -26,6 +26,7 @@ class Employee(models.Model):
 
     health_status = fields.Char(string="Health Status", store=True)
     mother_name = fields.Char(string="Mother Name", store=True)
+    mobile = fields.Char(string="Private Mobile", groups="hr.group_hr_user")
 
     blood_group = fields.Selection([
         ('ap', 'A+'),
@@ -44,7 +45,17 @@ class Employee(models.Model):
     passport_issue_date = fields.Date(string="Passport issue date", groups="hr.group_hr_user", tracking=True)
     
     national_id = fields.Char(string='National No', groups="hr.group_hr_user", tracking=True)
+    financial_id = fields.Char(string='Financial number', groups="hr.group_hr_user", tracking=True)
+    warranty_id = fields.Char(string='Warranty card number', groups="hr.group_hr_user", tracking=True)
 
+    family_registration_id = fields.Char(string='Family Registration Number', groups="hr.group_hr_user", tracking=True)
+    family_booklet_id = fields.Char(string='Family Booklet Number', groups="hr.group_hr_user", tracking=True)
+
+    issuing_booklet = fields.Char(string='Issuing authority', groups="hr.group_hr_user", tracking=True)
+    family_paper_id = fields.Char(string='Family paper number', groups="hr.group_hr_user", tracking=True)
+
+    family_id = fields.Many2one(
+        'hr.employee.family', 'Family', groups="hr.group_hr_user", tracking=True)
 
 
 
@@ -53,3 +64,20 @@ class EmployeeStatus(models.Model):
     _description = "Employee status"
 
     name = fields.Char(string="Employee Status", store=True)
+
+
+class EmployeeFamily(models.Model):
+    _name = 'hr.employee.family'
+    _description = "Employee Family"
+
+    name = fields.Char(string="Name", store=True)
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other')
+    ], groups="hr.group_hr_user", default="male", tracking=True)
+    birthday = fields.Date('Date of Birth', groups="hr.group_hr_user", tracking=True)
+
+    relation = fields.Char(string="Relative Relation", store=True)
+    relation_status = fields.Char(string="Social and occupational status", store=True)
+    national_id = fields.Char(string='National No', groups="hr.group_hr_user", tracking=True)
