9157da29b5b30e78e670b8424276988da346dd9f
med-sahara <55658971+med-sahara@users.noreply.github.com>
2020-01-23 00:48:42 +0200

    hr_employee_info

--- hr_employee_info/__manifest__.py
+++ hr_employee_info/__manifest__.py
@@ -3,10 +3,11 @@
     'name' : 'hr employee info',
     'version' : '1.0',
     'sequence': 1,
-    'depends' : ['hr','base'],
+    'depends' : ['hr','hr_contract','base'],
     'data': [
         'security/ir.model.access.csv',
         'views/hr_employee_info.xml',
+        'views/contract.xml',
     ],
     'installable': True,
 }
---
+++ hr_employee_info/models/contract.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+class Contract(models.Model):
+    _inherit = 'hr.contract'
+
+    hiring_date = fields.Date('Hiring Date', default=fields.Date.today,
+        help="hiring date.")
+    started_date = fields.Date('Work Started Date', default=fields.Date.today,
+        help="Work Started date.")
+    first_work_date = fields.Date('First Work Date', default=fields.Date.today,
+        help="First Work Date.")
+    position_type = fields.Many2one('hr.contract.positiontype', string="Position Type")
+
+
+    appointment_decision = fields.Char('Appointment Decision No.')
+
+class ContractPositionType(models.Model):
+    _name = 'hr.contract.positiontype'
+    _description = 'Contract Position Type'
+
+    name = fields.Char(string='Position Type', required=True)
