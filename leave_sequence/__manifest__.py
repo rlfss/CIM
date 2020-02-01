233e244b1f2c2d2564e39bec8cbb65c165e1af48
med-sahara <55658971+med-sahara@users.noreply.github.com>
2020-02-02 01:01:31 +0200

    leave_sequence

---
+++ leave_sequence/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name" : "Leave Sequence",
+    "version" : "1.0",
+    "category" : "hr",
+    "depends" : ["base","hr","hr_holidays"],
+    "data" : [
+        'view/leave.xml',
+        'data/sequence.xml',
+    ],
+    "installable": True
+}
