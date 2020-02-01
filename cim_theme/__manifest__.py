8a21f423edd6a3d9109cd2e3f9198b91f82fb1a1
med-sahara <55658971+med-sahara@users.noreply.github.com>
2020-02-01 17:16:56 +0200

    cim_theme

---
+++ cim_theme/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Theme CIM",
+    'version': '1.0',
+    'category': 'Theme',
+    'depends': ['website_crm'],
+    'data': [
+        'views/assets.xml',
+        'views/portal.xml',
+    ],
+}
