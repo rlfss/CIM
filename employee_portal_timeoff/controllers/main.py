336d31899859886b9d7aa6d57d164b022c20db73
med-sahara <55658971+med-sahara@users.noreply.github.com>
2020-06-27 01:54:38 +0300

    Update main.py

--- employee_portal_timeoff/controllers/main.py
+++ employee_portal_timeoff/controllers/main.py
@@ -10,8 +10,93 @@ from odoo.addons.portal.controllers.portal import CustomerPortal
 from odoo.tools.translate import _
 from odoo.exceptions import AccessError, MissingError
 import binascii
+import functools
+import logging
 
+import json
 
+import werkzeug.urls
+import werkzeug.utils
+from werkzeug.exceptions import BadRequest
+
+from odoo import api, http, SUPERUSER_ID, _
+from odoo.exceptions import AccessDenied
+from odoo.http import request
+from odoo import registry as registry_get
+
+from odoo.addons.auth_signup.controllers.main import AuthSignupHome as Home
+from odoo.addons.web.controllers.main import db_monodb, ensure_db, set_cookie_and_redirect, login_and_redirect
+
+def fragment_to_query_string(func):
+    @functools.wraps(func)
+    def wrapper(self, *a, **kw):
+        kw.pop('debug', False)
+        if not kw:
+            return """<html><head><script>
+                var l = window.location;
+                var q = l.hash.substring(1);
+                var r = l.pathname + l.search;
+                if(q.length !== 0) {
+                    var s = l.search ? (l.search === '?' ? '' : '&') : '?';
+                    r = l.pathname + l.search + s + q;
+                }
+                if (r == l.pathname) {
+                    r = '/';
+                }
+                window.location = r;
+            </script></head><body></body></html>"""
+        return func(self, *a, **kw)
+    return wrapper
+
+class OAuthController(http.Controller):
+
+    @http.route('/auth_oauth/signin', type='http', auth='none')
+    @fragment_to_query_string
+    def signin(self, **kw):
+        state = json.loads(kw['state'])
+        dbname = state['d']
+        if not http.db_filter([dbname]):
+            return BadRequest()
+        provider = state['p']
+        context = state.get('c', {})
+        registry = registry_get(dbname)
+        with registry.cursor() as cr:
+            try:
+                env = api.Environment(cr, SUPERUSER_ID, context)
+                credentials = env['res.users'].sudo().auth_oauth(provider, kw)
+                cr.commit()
+                action = state.get('a')
+                menu = state.get('m')
+                redirect = werkzeug.url_unquote_plus(state['r']) if state.get('r') else False
+                url = '/my/home'
+                if redirect:
+                    url = redirect
+                elif action:
+                    url = '/web#action=%s' % action
+                elif menu:
+                    url = '/web#menu_id=%s' % menu
+                resp = login_and_redirect(*credentials, redirect_url=url)
+                # Since /web is hardcoded, verify user has right to land on it
+                if werkzeug.urls.url_parse(resp.location).path == '/web' and not request.env.user.has_group('base.group_user'):
+                    resp.location = '/my/home'
+                return resp
+            except AttributeError:
+                # auth_signup is not installed
+                _logger.error("auth_signup not installed on database %s: oauth sign up cancelled." % (dbname,))
+                url = "/web/login?oauth_error=1"
+            except AccessDenied:
+                # oauth credentials not valid, user could be on a temporary session
+                _logger.info('OAuth2: access denied, redirect to main page in case a valid session exists, without setting cookies')
+                url = "/web/login?oauth_error=3"
+                redirect = werkzeug.utils.redirect(url, 303)
+                redirect.autocorrect_location_header = False
+                return redirect
+            except Exception as e:
+                # signup error
+                _logger.exception("OAuth2: %s" % str(e))
+                url = "/web/login?oauth_error=2"
+
+        return set_cookie_and_redirect(url)
 
 class WebsiteAccount(CustomerPortal):
 
