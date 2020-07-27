49814acf798c0cb0afe313d47ff565a062d29156
med-sahara <55658971+med-sahara@users.noreply.github.com>
2020-07-28 00:15:02 +0300

    Update home.js

--- leave_cim/static/src/js/home.js
+++ leave_cim/static/src/js/home.js
@@ -1,18 +1,15 @@
-odoo.define('leave_cim.home_icon', function (require) {
+odoo.define('systray.systray_odoo_referral', function (require) {
     "use strict";
     var localStorage = require('web.local_storage');
-    var SystrayMenu = require('web.SystrayMenu');
     var Widget = require('web.Widget');
 
     var ActionMenu = Widget.extend({
         template: 'leave_cim.home_icon',
         events: {
-            'click .home_icon': 'onclick_gifticon',
+            'click .home_icon': 'onclick_home_icon',
         },
 
-        start: function (parent) {
-
-        onclick_gifticon: function () {
+        onclick_home_icon: function () {
             var self = this;
             this._rpc({
                 route: '/my/home'
@@ -21,7 +18,4 @@ odoo.define('leave_cim.home_icon', function (require) {
             });
         },
     });
-
-    SystrayMenu.Items.push(ActionMenu);
-    return ActionMenu;
 });
