6588461eb53e0dfda6eb9441a57d2b866413be8f
med-sahara <55658971+med-sahara@users.noreply.github.com>
2020-07-28 01:03:51 +0300

    Update internal_memo.py

--- internal_memo/models/internal_memo.py
+++ internal_memo/models/internal_memo.py
@@ -13,7 +13,8 @@ class InternalMemo(models.Model):
     manager_id = fields.Many2one('hr.employee', 'Manager')
     to = fields.Char(string='To')
     via = fields.Char(string='Via')
-    message = fields..Html(string='Message')
+    message = fields.Html('Message')
+
     date = fields.Date(string="Date", readonly=True, default=fields.Date.context_today)
 
     @api.onchange('employee_id')
