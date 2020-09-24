d6230d41301dcc2ce851219df758139bb2f1606b
Your Name <you@example.com>
2020-09-24 05:55:59 +0200

    fix_bugs2

--- cim_theme/models/res_lang.py
+++ cim_theme/models/res_lang.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 import odoo
+from datetime import datetime
+
 
 class Language(models.Model):
     _inherit = 'res.lang'
@@ -16,3 +18,11 @@ class Language(models.Model):
     def write(self, vals):
         self._get_languages_dir.clear_cache(self)
         return super(Language, self).write(vals)
+
+class EmployeeDate(models.Model):
+    _inherit = 'hr.employee'
+
+    # current_date = fields.Date('Date current action', required=False, readonly=False, select=True
+    #                             ,default=lambda self: fields.datetime.now())
+    current_time = fields.Datetime('Date current action', required=False, readonly=False, select=True
+                                ,default=fields.Datetime.now)
