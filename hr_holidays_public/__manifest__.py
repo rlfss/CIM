a6e1dd32c4eb7511307d2d75d2656d3e7cc1d17f
Your Name <you@example.com>
2020-10-07 17:18:11 +0200

    fix bugs

--- hr_holidays_public/__manifest__.py
+++ hr_holidays_public/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'HR Holidays Public',
+    'name': 'HR Holidays Public ',
     'version': '12.0.1.0.3',
     'license': 'AGPL-3',
     'category': 'Human Resources',
