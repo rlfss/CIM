fd6ab7988828268c24576127b359eeb71db4cab1
Your Name <you@example.com>
2020-10-22 23:30:03 +0200

    fix bugs

--- hr_employee_info/models/hr_employee_info.py
+++ hr_employee_info/models/hr_employee_info.py
@@ -117,11 +117,12 @@ class Employee(models.Model):
 
     appointment_decision = fields.Char('Appointment Decision No.', compute='_compute_contract_data', readonly=True,stored=True)
 
-    employee_age = fields.Integer(string="Age")
+    employee_age = fields.Integer(string="Age",compute='_compute_employee_birthday',stored=True)
 
 
-    def compute_employee_birthday(self):
-        print("aaaaaaaaaa")
+    @api.depends('birthday')
+    def _compute_employee_birthday(self):
+        print("aaaaa")
         if self.birthday:
             today = date.today()
             age = today.year - self.birthday.year - (
